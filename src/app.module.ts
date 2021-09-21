5b769bd12c9e4cf6b97114443aebf06241aba51f
Gordey.Babich <Gordey.Babich@x5.ru>
2021-09-21 20:19:54 +0300

    added serve static

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { StagesModule } from './stages/stages.module';
 import { ProjectsModule } from './projects/projects.module';
 import { StageResultsModule } from './stage-results/stage-results.module';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'front'),
+    }),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
     StagesModule,
